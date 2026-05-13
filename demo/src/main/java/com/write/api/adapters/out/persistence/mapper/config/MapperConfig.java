9c1c3d7d7a72c49ec8073c2945d11c9e32c9ec7c
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-13 09:30:36 -0300

    Set up MapperConfig

---
+++ demo/src/main/java/com/write/api/adapters/out/persistence/mapper/config/MapperConfig.java
@@ -0,0 +1,16 @@
+package com.write.api.adapters.out.persistence.mapper.config;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MapperConfig {
+
+    public boolean map(Byte value) {
+        return value != null && value == 1;
+    }
+
+    public Byte map(boolean value) {
+        return (byte) (value ? 1 : 0);
+    }
+
+}
