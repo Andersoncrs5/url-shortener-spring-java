a6a17d4ff0c259329d79e8fb3bdc1e7bbd99ec7b
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-13 09:28:24 -0300

    Set up HelpRepositoryTest

---
+++ demo/src/test/java/com/write/api/adapters/out/persistence/help/HelpRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.write.api.adapters.out.persistence.help;
+
+import com.write.api.adapters.out.persistence.repository.JooqUserRepository;
+import com.write.api.core.domain.model.UserModel;
+import com.write.api.core.domain.service.SnowflakeIdGenerator;
+
+public class HelpRepositoryTest {
+
+    private final SnowflakeIdGenerator generator;
+    private final JooqUserRepository repository;
+
+    public HelpRepositoryTest(
+            SnowflakeIdGenerator generator,
+            JooqUserRepository repository
+    ) {
+        this.generator = generator;
+        this.repository = repository;
+    }
+
+    public UserModel create() {
+        UserModel user = new UserModel();
+        user.setName("John Doe" + generator.nextId());
+        user.setEmail("john" + generator.nextId() + "@example.com");
+        user.setPasswordHash("54356435645625467245425");
+        user.setActive(true);
+
+        return repository.insert(user);
+    }
+}
