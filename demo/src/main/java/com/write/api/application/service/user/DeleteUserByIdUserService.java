9ad1812af8cece687da799611b1535e0028b52ba
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-13 14:08:18 -0300

    Set up DeleteUserByIdUserService

---
+++ demo/src/main/java/com/write/api/application/service/user/DeleteUserByIdUserService.java
@@ -0,0 +1,26 @@
+package com.write.api.application.service.user;
+
+import com.write.api.application.shared.Result;
+import com.write.api.ports.in.user.DeleteByIdUserUseCase;
+import com.write.api.ports.out.repository.IUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DeleteUserByIdUserService implements DeleteByIdUserUseCase {
+
+    private final IUserRepository repository;
+
+    @Override
+    public Result<Void> deleteById(Long id) {
+        int deleted = repository.deleteById(id);
+
+        if (deleted == 0) {
+            return Result.failure("User not found", 404);
+        }
+
+        return Result.success(200);
+    }
+
+}
