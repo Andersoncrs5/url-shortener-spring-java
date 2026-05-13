7101819cfa0963ba864f35bd01070ae6e1e86744
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-13 09:28:05 -0300

    Impled logic to CreateUserService

--- demo/src/main/java/com/write/api/application/service/user/CreateUserService.java
+++ demo/src/main/java/com/write/api/application/service/user/CreateUserService.java
@@ -1,10 +1,12 @@
 package com.write.api.application.service.user;
 
 import com.write.api.application.shared.Result;
+import com.write.api.core.domain.exception.InternalServerErrorException;
 import com.write.api.core.domain.model.UserModel;
 import com.write.api.ports.in.user.CreateUserUseCase;
 import com.write.api.ports.out.repository.IUserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,7 +17,38 @@ public class CreateUserService implements CreateUserUseCase {
 
     @Override
     public Result<UserModel> create(UserModel user) {
-        return null;
+        try {
+            var created = repository.insert(user);
+
+            return Result.success(created, 201);
+        } catch (DataIntegrityViolationException e) {
+
+            String message = e.getMostSpecificCause().getMessage();
+
+            if (message != null && message.contains("uk_user_email")) {
+                return  Result.failure(
+                        "Email already exists",
+                        409
+                );
+            }
+
+            if (message != null && message.contains("uk_user_name")) {
+                return  Result.failure(
+                        "Username already exists",
+                        409
+                );
+            }
+
+            return  Result.failure(
+                    "Database integrity error: " + message,
+                    400
+            );
+
+        } catch (Exception e) {
+            throw new InternalServerErrorException(
+                    e.getMessage()
+            );
+        }
     }
 
 }
