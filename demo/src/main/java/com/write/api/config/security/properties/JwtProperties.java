097e9973a648c88c52936db7be60103781d2f151
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-18 15:36:57 -0300

    Set up JwtProperties

---
+++ demo/src/main/java/com/write/api/config/security/properties/JwtProperties.java
@@ -0,0 +1,27 @@
+package com.write.api.config.security.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Getter @Setter @Component
+@ConfigurationProperties(prefix = "spring.security")
+public class JwtProperties {
+
+    private Exp exp = new Exp();
+    private Jwt jwt = new Jwt();
+
+    @Getter
+    @Setter
+    public static class Exp {
+        private long token;
+        private long refresh;
+    }
+
+    @Getter
+    @Setter
+    public static class Jwt {
+        private String secret;
+    }
+}
