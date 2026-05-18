dede12e0e04e863c953433b07aebeab0f1118543
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-18 15:36:08 -0300

    Added UserPrincipal

---
+++ demo/src/main/java/com/write/api/config/security/classes/UserPrincipal.java
@@ -0,0 +1,21 @@
+package com.write.api.config.security.classes;
+
+
+import com.write.api.core.domain.model.UserModel;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+@Getter
+public class UserPrincipal extends User {
+    private final Long id;
+    private final UserModel user;
+
+    public UserPrincipal(Long id, String username, String password, Collection<? extends GrantedAuthority> authorities, UserModel user) {
+        super(username, password, authorities);
+        this.id = id;
+        this.user = user;
+    }
+}
