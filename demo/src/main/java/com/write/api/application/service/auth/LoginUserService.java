8212b8daeaa96974e91d8844cb0982437234018e
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-20 09:09:05 -0300

    New packages

---
+++ demo/src/main/java/com/write/api/application/service/auth/LoginUserService.java
@@ -0,0 +1,71 @@
+package com.write.api.application.service.auth;
+
+import com.write.api.application.dto.auth.AuthTokenResponseDTO;
+import com.write.api.application.dto.user.LoginUserDTO;
+import com.write.api.application.shared.Result;
+import com.write.api.config.security.jwt.TokenService;
+import com.write.api.core.domain.model.UserModel;
+import com.write.api.ports.in.auth.LoginUserUseCase;
+import com.write.api.ports.out.repository.IUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class LoginUserService implements LoginUserUseCase {
+
+    private final TokenService service;
+    private final PasswordEncoder passwordEncoder;
+    private final IUserRepository repository;
+
+    @Override
+    public Result<AuthTokenResponseDTO> login(LoginUserDTO dto) {
+        UserModel user = repository.findByEmailIgnoreCase(dto.email()).orElse(null);
+
+        if (user == null) {
+            return Result.failure(401, "Access denied");
+        }
+
+        if (user.getBlockedAt() != null &&
+                user.getBlockedAt().isAfter(LocalDateTime.now())) {
+
+            return Result.failure(423, "User blocked");
+        }
+
+        boolean matches = this.passwordEncoder.matches(
+                dto.password(),
+                user.getPasswordHash()
+        );
+
+        if (!matches) {
+            user.sumAttemptLogin();
+
+            if (user.getAttemptsLoginFailed() >= 3) {
+                user.setBlockedAt(LocalDateTime.now().plusHours(4));
+            }
+
+            repository.save(user);
+
+            if (user.getAttemptsLoginFailed() >= 3) {
+                return Result.failure(423, "User blocked");
+            }
+
+            return Result.failure(401, "Access denied");
+        }
+
+        user.setAttemptsLoginFailed(0);
+        user.setBlockedAt(null);
+        user.setLastLoginAt(LocalDateTime.now());
+
+        repository.save(user);
+
+        AuthTokenResponseDTO tokens = this.service.createTokens(user);
+
+        return Result.success(tokens);
+    }
+
+
+}
