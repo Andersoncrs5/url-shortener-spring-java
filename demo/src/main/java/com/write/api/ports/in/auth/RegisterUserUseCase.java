69801bb6220b1e39e9b055a3826c17bd1c8b453e
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-20 16:36:28 -0300

    Added method execute

--- demo/src/main/java/com/write/api/ports/in/auth/RegisterUserUseCase.java
+++ demo/src/main/java/com/write/api/ports/in/auth/RegisterUserUseCase.java
@@ -1,4 +1,9 @@
 package com.write.api.ports.in.auth;
 
+import com.write.api.application.dto.auth.AuthTokenResponseDTO;
+import com.write.api.application.dto.user.CreateUserDTO;
+import com.write.api.application.shared.Result;
+
 public interface RegisterUserUseCase {
+    Result<AuthTokenResponseDTO> execute(CreateUserDTO dto);
 }
