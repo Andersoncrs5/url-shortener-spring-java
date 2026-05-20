e5045c1de560aada7637830d91d1278b9f092b66
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-20 16:36:18 -0300

    Added ResponseHttp

---
+++ demo/src/main/java/com/write/api/adapters/in/web/shared/response/ResponseHttp.java
@@ -0,0 +1,95 @@
+package com.write.api.adapters.in.web.shared.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record ResponseHttp<T>(
+        T data,
+
+        @NotBlank
+        String message,
+
+        @NotBlank
+        String traceId,
+
+        @NotNull
+        Integer version,
+
+        @NotNull
+        boolean status,
+
+        @NotNull
+        OffsetDateTime timestamp
+) {
+
+    public static <T> ResponseHttp<T> success(T data, String message) {
+        return new ResponseHttp<>(
+                data,
+                message,
+                UUID.randomUUID().toString(),
+                1,
+                true,
+                OffsetDateTime.now()
+        );
+    }
+
+    public static <T> ResponseHttp<T> success(String message, String traceId) {
+        return new ResponseHttp<>(
+                null,
+                message,
+                traceId,
+                1,
+                true,
+                OffsetDateTime.now()
+        );
+    }
+
+    public static <T> ResponseHttp<T> success(T data, String message, String traceId) {
+        return new ResponseHttp<>(
+                data,
+                message,
+                traceId,
+                1,
+                true,
+                OffsetDateTime.now()
+        );
+    }
+
+    public static <T> ResponseHttp<T> success(T data, String message, int version) {
+        return new ResponseHttp<>(
+                data,
+                message,
+                UUID.randomUUID().toString(),
+                version,
+                true,
+                OffsetDateTime.now()
+        );
+    }
+
+    public static <T> ResponseHttp<T> error(String message) {
+        return new ResponseHttp<>(
+                null,
+                message,
+                UUID.randomUUID().toString(),
+                1,
+                false,
+                OffsetDateTime.now()
+        );
+    }
+
+    public static <T> ResponseHttp<T> error(String message, String traceId) {
+        return new ResponseHttp<>(
+                null,
+                message,
+                traceId,
+                1,
+                false,
+                OffsetDateTime.now()
+        );
+    }
+}
