27e926d5af567e25f8b2c9a94b969a143f97d663
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-20 16:52:19 -0300

    New use case

---
+++ demo/src/main/java/com/write/api/application/service/auth/RefreshTokenService.java
@@ -0,0 +1,42 @@
+package com.write.api.application.service.auth;
+
+import com.write.api.application.dto.auth.AuthTokenResponseDTO;
+import com.write.api.application.shared.Result;
+import com.write.api.config.security.jwt.TokenService;
+import com.write.api.core.domain.model.UserModel;
+import com.write.api.ports.in.auth.RefreshTokenUseCase;
+import com.write.api.ports.out.repository.IUserRepository;
+import com.write.api.shared.tx.ResultTransaction;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RefreshTokenService implements RefreshTokenUseCase {
+
+    private final IUserRepository repository;
+    private final TokenService service;
+
+    @Override
+    @ResultTransaction
+    public Result<AuthTokenResponseDTO> execute(String refreshToken) {
+        Result<String> validated = this.service.validateTokenV2(refreshToken);
+        if (validated.isFailure()) return Result.failure(validated.getStatusCode(), validated.getMessage());
+
+        UserModel user = this.repository.findByRefreshToken(refreshToken).orElse(null);
+
+        if (user == null) return Result.failure(404, "User not found");
+
+        if (!validated.getValue().equals(user.getId().toString())) {
+            return Result.failure(401, "Token mismatch");
+        }
+
+        AuthTokenResponseDTO tokens = this.service.createTokens(user);
+
+        user.setRefreshToken(tokens.refreshToken());
+        repository.save(user);
+
+        return Result.success(tokens);
+    }
+
+}
--- demo/src/main/java/com/write/api/ports/in/auth/RefreshTokenUseCase.java
+++ demo/src/main/java/com/write/api/ports/in/auth/RefreshTokenUseCase.java
@@ -1,4 +1,8 @@
 package com.write.api.ports.in.auth;
 
+import com.write.api.application.dto.auth.AuthTokenResponseDTO;
+import com.write.api.application.shared.Result;
+
 public interface RefreshTokenUseCase {
+    Result<AuthTokenResponseDTO> execute(String refreshToken);
 }
