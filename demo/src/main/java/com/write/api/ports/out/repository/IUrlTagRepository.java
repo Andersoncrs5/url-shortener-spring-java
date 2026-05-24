d1773a705ae108c8113c8a1091b3a97982f851f8
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-24 20:04:21 -0300

    Set up IUrlTagRepository

---
+++ demo/src/main/java/com/write/api/ports/out/repository/IUrlTagRepository.java
@@ -0,0 +1,6 @@
+package com.write.api.ports.out.repository;
+
+import com.write.api.core.domain.model.UrlTagModel;
+
+public interface IUrlTagRepository extends CrudRepository<UrlTagModel, Long> {
+}
