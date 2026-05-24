f3e5aebeb4f52397d6f124516975d6cba2b89bfe
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-24 16:47:47 -0300

    new method success

--- demo/src/main/java/com/write/api/application/shared/Result.java
+++ demo/src/main/java/com/write/api/application/shared/Result.java
@@ -25,6 +25,10 @@ public class Result<T> {
         return new Result<>(value, null, 200, true);
     }
 
+    public static <T> Result<T> success() {
+        return new Result<>(null, null, 200, true);
+    }
+
     public static <T> Result<T> success(T value, int statusCode) {
         return new Result<>(value, null, statusCode, true);
     }
