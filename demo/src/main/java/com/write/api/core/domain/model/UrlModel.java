93c7905dc88319d61ce8f231ea0def142ba3cf3a
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-24 19:40:46 -0300

    Added annotations

--- demo/src/main/java/com/write/api/core/domain/model/UrlModel.java
+++ demo/src/main/java/com/write/api/core/domain/model/UrlModel.java
@@ -2,28 +2,33 @@ package com.write.api.core.domain.model;
 
 import com.write.api.core.domain.enums.UrlAccessTypeEnum;
 import com.write.api.core.domain.enums.UrlStatusEnum;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class UrlModel {
-
-    private Long id;
-    private Long version;
-    private Long userId;
-    private String shortCode;
-    private String description;
-    private String faviconUrl;
-    private String originalUrl;
-    private String title;
-    private String domain;
-    private UrlStatusEnum status;
-    private UrlAccessTypeEnum accessType;
-    private String passwordHash;
-    private boolean customAlias;
-    private LocalDateTime deletedAt;
-    private LocalDateTime expiresAt;
-    private LocalDateTime lastAccessAt;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
-
+    Long id;
+    Long version;
+    Long userId;
+    String shortCode;
+    String description;
+    String faviconUrl;
+    String originalUrl;
+    String title;
+    String domain;
+    UrlStatusEnum status;
+    UrlAccessTypeEnum accessType;
+    String passwordHash;
+    boolean customAlias;
+    LocalDateTime deletedAt;
+    LocalDateTime expiresAt;
+    LocalDateTime lastAccessAt;
+    LocalDateTime createdAt;
+    LocalDateTime updatedAt;
 }
