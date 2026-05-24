3c85fff21ea298ecb07968e987ae91290ebd37ac
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-24 20:17:48 -0300

    Set up logic to delete url tag

---
+++ demo/src/main/java/com/write/api/application/service/urlTag/DeleteByIdService.java
@@ -0,0 +1,28 @@
+package com.write.api.application.service.urlTag;
+
+import com.write.api.application.shared.Result;
+import com.write.api.ports.in.urlTag.DeleteByIdUseCase;
+import com.write.api.ports.out.repository.IUrlTagRepository;
+import com.write.api.shared.tx.ResultTransaction;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class DeleteByIdService implements DeleteByIdUseCase {
+
+    private final IUrlTagRepository repository;
+
+    @Override
+    @ResultTransaction
+    public Result<Void> execute(Long id) {
+        int deleted = repository.deleteById(id);
+
+        if (deleted == 0) {
+            return Result.failure(404, "Url Tag not found");
+        }
+
+        return Result.success();
+    }
+
+}
