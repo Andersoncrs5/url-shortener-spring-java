a691d1ac6d3e7607899c55db0ddea886a0876e4f
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-26 14:13:06 -0300

    .

--- demo/target/generated-sources/annotations/com/write/api/adapters/in/web/mapper/UrlTagMapperImpl.java
+++ demo/target/generated-sources/annotations/com/write/api/adapters/in/web/mapper/UrlTagMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-05-24T17:12:50-0300",
+    date = "2026-05-25T14:35:07-0300",
     comments = "version: 1.6.3, compiler: javac, environment: Java 25.0.2 (Eclipse Adoptium)"
 )
 @Component
