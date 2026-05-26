235ef65d860dbf3912c1768ecd184f6fadd059d3
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-26 14:18:26 -0300

    Set up DeleteUrlByIdSoft

---
+++ demo/src/main/java/com/write/api/ports/in/url/DeleteUrlByIdSoftUseCase.java
@@ -0,0 +1,8 @@
+package com.write.api.ports.in.url;
+
+import com.write.api.application.shared.Result;
+import com.write.api.shared.validation.snowflake.IsId;
+
+public interface DeleteUrlByIdSoftUseCase {
+    Result<Void> execute(@IsId Long id);
+}
