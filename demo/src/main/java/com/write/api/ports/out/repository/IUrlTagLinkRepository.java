1b5a9c759012d8c21fc0784e97c1aa6b30101c61
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-26 18:32:42 -0300

    Set up IUrlTagLinkRepository

---
+++ demo/src/main/java/com/write/api/ports/out/repository/IUrlTagLinkRepository.java
@@ -0,0 +1,6 @@
+package com.write.api.ports.out.repository;
+
+import com.write.api.core.domain.model.UrlTagLinkModel;
+
+public interface IUrlTagLinkRepository extends CrudRepository<UrlTagLinkModel, Long> {
+}
