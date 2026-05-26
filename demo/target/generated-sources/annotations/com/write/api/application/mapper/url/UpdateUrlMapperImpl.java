7a1f820a481a496c4ca81ba4eae096146873b151
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-26 14:19:21 -0300

    .

---
+++ demo/target/generated-sources/annotations/com/write/api/application/mapper/url/UpdateUrlMapperImpl.java
@@ -0,0 +1,47 @@
+package com.write.api.application.mapper.url;
+
+import com.write.api.application.dto.url.UpdateUrlDTO;
+import com.write.api.core.domain.model.UrlModel;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2026-05-25T15:45:26-0300",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 25.0.2 (Eclipse Adoptium)"
+)
+@Component
+public class UpdateUrlMapperImpl implements UpdateUrlMapper {
+
+    @Override
+    public void update(UpdateUrlDTO dto, UrlModel model) {
+        if ( dto == null ) {
+            return;
+        }
+
+        if ( dto.description() != null ) {
+            model.setDescription( dto.description() );
+        }
+        if ( dto.faviconUrl() != null ) {
+            model.setFaviconUrl( dto.faviconUrl() );
+        }
+        if ( dto.originalUrl() != null ) {
+            model.setOriginalUrl( dto.originalUrl() );
+        }
+        if ( dto.title() != null ) {
+            model.setTitle( dto.title() );
+        }
+        if ( dto.domain() != null ) {
+            model.setDomain( dto.domain() );
+        }
+        if ( dto.status() != null ) {
+            model.setStatus( dto.status() );
+        }
+        if ( dto.accessType() != null ) {
+            model.setAccessType( dto.accessType() );
+        }
+        if ( dto.expiresAt() != null ) {
+            model.setExpiresAt( dto.expiresAt() );
+        }
+    }
+}
