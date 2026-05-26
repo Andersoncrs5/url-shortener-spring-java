1c2348a1a608e54c52692c6815d5b4aa93b2e12e
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-26 18:21:11 -0300

    Set up UrlAccessRuleTypeEnum

---
+++ demo/src/main/java/com/write/api/core/domain/enums/UrlAccessRuleTypeEnum.java
@@ -0,0 +1,24 @@
+package com.write.api.core.domain.enums;
+
+public enum UrlAccessRuleTypeEnum {
+
+    PASSWORD,
+
+    COUNTRY_ALLOW,
+
+    COUNTRY_BLOCK,
+
+    IP_ALLOW,
+
+    IP_BLOCK,
+
+    USER_AGENT_BLOCK,
+
+    RATE_LIMIT,
+
+    REQUIRE_AUTH,
+
+    EXPIRES_AT,
+
+    MAX_CLICKS
+}
