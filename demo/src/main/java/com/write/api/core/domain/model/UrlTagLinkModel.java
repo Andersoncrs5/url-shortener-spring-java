a34d8be32c4455263ca0d839de1f078954aeaa40
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-26 18:26:26 -0300

    Added columns

--- demo/src/main/java/com/write/api/core/domain/model/UrlTagLinkModel.java
+++ demo/src/main/java/com/write/api/core/domain/model/UrlTagLinkModel.java
@@ -1,11 +1,28 @@
 package com.write.api.core.domain.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
 public class UrlTagLinkModel {
 
     private Long id;
+
     private Long urlId;
     private Long tagId;
+
+    private Short sortOrder;
+
+    private String note;
+
+    private boolean primaryTag;
+
+    private Long createdBy;
+
     private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
 }
