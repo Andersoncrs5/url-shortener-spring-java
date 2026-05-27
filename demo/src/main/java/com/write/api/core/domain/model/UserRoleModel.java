85c394825d56f3bf567b1878fb9f9347fda0cd34
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-27 17:37:59 -0300

    New class to domain UserRoleModel

---
+++ demo/src/main/java/com/write/api/core/domain/model/UserRoleModel.java
@@ -0,0 +1,10 @@
+package com.write.api.core.domain.model;
+
+import com.write.api.core.domain.model.shared.BaseModel;
+
+public class UserRoleModel extends BaseModel {
+
+    private Long userId;
+
+    private Long roleId;
+}
