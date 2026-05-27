37ace870c68a6996783c3a3c537762be5e6b504b
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-27 17:16:13 -0300

    Set up MatchTypeEnum

---
+++ demo/src/main/java/com/write/api/core/domain/enums/MatchTypeEnum.java
@@ -0,0 +1,12 @@
+package com.write.api.core.domain.enums;
+
+public enum MatchTypeEnum {
+    EXACT,
+    PARTIAL,
+    STARTS_WITH,
+    ENDS_WITH,
+    CONTAINS,
+    ANY,
+    NOT,
+    WILDCARD
+}
