9d2cf409d97557e05694419a5de9b8511fe8bedc
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-27 17:16:17 -0300

    Set up OperatingSystemEnum

---
+++ demo/src/main/java/com/write/api/core/domain/enums/OperatingSystemEnum.java
@@ -0,0 +1,14 @@
+package com.write.api.core.domain.enums;
+
+public enum OperatingSystemEnum {
+    WINDOWS,
+    MACOS,
+    LINUX,
+    ANDROID,
+    IOS,
+    IPADOS,
+    CHROME_OS,
+    TVOS,
+    WATCHOS,
+    OTHER
+}
