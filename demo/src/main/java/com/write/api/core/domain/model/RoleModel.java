a16260682026b8549c466f9de1f4e05b618756b9
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-27 17:38:09 -0300

    New class to domain RoleModel

---
+++ demo/src/main/java/com/write/api/core/domain/model/RoleModel.java
@@ -0,0 +1,9 @@
+package com.write.api.core.domain.model;
+
+import com.write.api.core.domain.model.shared.BaseModel;
+
+public class RoleModel extends BaseModel {
+    private String name;
+    private String description;
+    private boolean active;
+}
