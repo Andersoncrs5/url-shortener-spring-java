5794a77dac1cc1f63891ddf2f3017047db5b36b6
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-27 17:40:37 -0300

    Improvements add more columns and extended BaseModel

--- demo/src/main/java/com/write/api/core/domain/model/UrlRedirectRuleModel.java
+++ demo/src/main/java/com/write/api/core/domain/model/UrlRedirectRuleModel.java
@@ -1,18 +1,36 @@
 package com.write.api.core.domain.model;
 
+import com.write.api.core.domain.enums.BrowserEnum;
+import com.write.api.core.domain.enums.ContinentEnum;
+import com.write.api.core.domain.enums.MatchTypeEnum;
+import com.write.api.core.domain.enums.OperatingSystemEnum;
+import com.write.api.core.domain.model.shared.BaseModel;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
-public class UrlRedirectRuleModel {
+@Getter
+@Setter
+public class UrlRedirectRuleModel extends BaseModel {
 
-    private Long id;
     private Long urlId;
+
     private String countryCode;
-    private String deviceType;
+    private String region;
+    private ContinentEnum continent;
+
+    private OperatingSystemEnum os;
+    private BrowserEnum browser;
+
+    private MatchTypeEnum matchType;
+
     private String redirectUrl;
+    private String ruleHash;
     private Integer priority;
+
     private boolean active;
+
     private LocalDateTime startAt;
     private LocalDateTime endAt;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
 }
