45fba1dd5b761b74f6a79370d76b361b583f907f
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-27 16:47:01 -0300

    Set up ContinentEnum

---
+++ demo/src/main/java/com/write/api/core/domain/enums/ContinentEnum.java
@@ -0,0 +1,11 @@
+package com.write.api.core.domain.enums;
+
+public enum ContinentEnum {
+    AFRICA,
+    ANTARCTICA,
+    ASIA,
+    EUROPE,
+    NORTH_AMERICA,
+    SOUTH_AMERICA,
+    OCEANIA
+}
