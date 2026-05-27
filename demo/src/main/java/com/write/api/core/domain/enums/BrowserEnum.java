919f8611737db227cb005f23105c3229a343a796
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-27 16:46:57 -0300

    Set up BrowserEnum

---
+++ demo/src/main/java/com/write/api/core/domain/enums/BrowserEnum.java
@@ -0,0 +1,16 @@
+package com.write.api.core.domain.enums;
+
+public enum BrowserEnum {
+    CHROME,
+    CHROMIUM,
+    EDGE,
+    FIREFOX,
+    SAFARI,
+    OPERA,
+    BRAVE,
+    VIVALDI,
+    SAMSUNG_INTERNET,
+    UC_BROWSER,
+    OTHER,
+    BOT
+}
