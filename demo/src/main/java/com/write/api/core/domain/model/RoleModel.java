3b1fad71d0e39c0b0ff89a7fe6aa2027370b7e72
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-28 19:29:31 -0300

    Added Getter and Setter

--- demo/src/main/java/com/write/api/core/domain/model/RoleModel.java
+++ demo/src/main/java/com/write/api/core/domain/model/RoleModel.java
@@ -1,7 +1,11 @@
 package com.write.api.core.domain.model;
 
 import com.write.api.core.domain.model.shared.BaseModel;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class RoleModel extends BaseModel {
     private String name;
     private String description;
