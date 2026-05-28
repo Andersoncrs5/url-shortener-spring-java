acf6f9e4584003f610141d2c3ecc55f9d0edf07c
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-28 19:32:05 -0300

    Added Docs to UrlRedirectRuleControllerDocs

---
+++ demo/src/main/java/com/write/api/adapters/in/web/controller/docs/UrlRedirectRuleControllerDocs.java
@@ -0,0 +1,38 @@
+package com.write.api.adapters.in.web.controller.docs;
+
+import com.write.api.adapters.in.web.shared.response.ResponseHttp;
+import com.write.api.application.dto.urlRedirectRule.CreateUrlRedirectRuleDTO;
+import com.write.api.application.dto.urlRedirectRule.UpdateUrlRedirectRuleDTO;
+import com.write.api.application.dto.urlRedirectRule.UrlRedirectRuleDTO;
+import com.write.api.infrastructure.config.api.idempotent.Idempotent;
+import com.write.api.shared.validation.snowflake.IsId;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+public interface UrlRedirectRuleControllerDocs {
+
+    @Idempotent
+    @PostMapping
+    ResponseEntity<ResponseHttp<UrlRedirectRuleDTO>> create(
+            @RequestBody @Valid CreateUrlRedirectRuleDTO dto,
+            @RequestHeader("X-Idempotency-Key") @NotBlank String idempotencyKey
+    );
+
+    @Idempotent
+    @DeleteMapping("/{id}")
+    ResponseEntity<ResponseHttp<?>> delete(
+            @PathVariable @IsId Long id,
+            @RequestHeader("X-Idempotency-Key") @NotBlank String idempotencyKey
+    );
+
+    @Idempotent
+    @PatchMapping("/{id}")
+    ResponseEntity<ResponseHttp<UrlRedirectRuleDTO>> update(
+            @PathVariable @IsId Long id,
+            @RequestBody @Valid UpdateUrlRedirectRuleDTO dto,
+            @RequestHeader("X-Idempotency-Key") @NotBlank String idempotencyKey
+    );
+
+}
