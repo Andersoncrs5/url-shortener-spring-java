ba626ad6456bf343b2e5a1587c9c83ed98c5317c
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-28 19:33:55 -0300

    Set up RoleSeeder

---
+++ demo/src/main/java/com/write/api/bootstrap/RoleSeeder.java
@@ -0,0 +1,62 @@
+package com.write.api.bootstrap;
+
+import com.write.api.core.domain.model.RoleModel;
+import com.write.api.ports.out.repository.IRoleRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jspecify.annotations.NonNull;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class RoleSeeder implements ApplicationRunner {
+
+    private final IRoleRepository repository;
+
+    @Override
+    @Transactional
+    public void run(@NonNull ApplicationArguments args) {
+
+        createIfNotExists(
+                "SUPER_ADMIN",
+                "Super administrator"
+        );
+
+        createIfNotExists(
+                "ADMIN",
+                "System administrator"
+        );
+
+        createIfNotExists(
+                "USER",
+                "Default user"
+        );
+
+        createIfNotExists(
+                "VIEWER",
+                "Viewer of URLs"
+        );
+    }
+
+    private void createIfNotExists(
+            String name,
+            String description
+    ) {
+        if (repository.existsByNameIgnoreCase(name)) {
+            log.info("{} already exists", "Role " + name);
+            return;
+        }
+
+        RoleModel role = new RoleModel();
+        role.setName(name);
+        role.setDescription(description);
+        role.setActive(true);
+
+        RoleModel inserted = repository.insert(role);
+        log.info("Role {} created with success", inserted.getName());
+    }
+}
