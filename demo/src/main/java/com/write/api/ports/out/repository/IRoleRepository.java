e1e3ef3a3c6e857ad805d747b7afcb85e916390e
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-28 16:15:51 -0300

    New method existsByNameIgnoreCase

--- demo/src/main/java/com/write/api/ports/out/repository/IRoleRepository.java
+++ demo/src/main/java/com/write/api/ports/out/repository/IRoleRepository.java
@@ -2,6 +2,8 @@ package com.write.api.ports.out.repository;
 
 import com.write.api.core.domain.model.RoleModel;
 import com.write.api.ports.out.repository.shared.CrudRepository;
+import jakarta.validation.constraints.NotBlank;
 
 public interface IRoleRepository extends CrudRepository<RoleModel, Long> {
+    boolean existsByNameIgnoreCase(@NotBlank String name);
 }
