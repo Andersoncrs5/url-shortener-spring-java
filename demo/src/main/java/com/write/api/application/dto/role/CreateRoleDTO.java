057f4086d2e0aa66335bf5bec49744559e03a755
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-28 16:13:31 -0300

    Added validation

--- demo/src/main/java/com/write/api/application/dto/role/CreateRoleDTO.java
+++ demo/src/main/java/com/write/api/application/dto/role/CreateRoleDTO.java
@@ -1,8 +1,19 @@
 package com.write.api.application.dto.role;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public record CreateRoleDTO(
+
+        @NotBlank(message = "name is required")
+        @Size(max = 100, message = "name exceeded 100 characters")
         String name,
+
+        @NotBlank(message = "description is required")
+        @Size(max = 5000, message = "description exceeded 5000 characters")
         String description,
+
         boolean active
+
 ) {
 }
