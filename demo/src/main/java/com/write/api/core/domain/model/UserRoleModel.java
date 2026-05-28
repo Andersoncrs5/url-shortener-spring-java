0b6410e9c482e4886ade5881f8879e7ec8f45367
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-28 19:30:20 -0300

    Added Getter and Setter

--- demo/src/main/java/com/write/api/core/domain/model/UserRoleModel.java
+++ demo/src/main/java/com/write/api/core/domain/model/UserRoleModel.java
@@ -1,10 +1,13 @@
 package com.write.api.core.domain.model;
 
 import com.write.api.core.domain.model.shared.BaseModel;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class UserRoleModel extends BaseModel {
-
     private Long userId;
-
     private Long roleId;
+    private Long assignedByUserId;
 }
