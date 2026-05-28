c26f56ba3ae8f857e8a148b185b79e9b7ecbcc23
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-28 16:17:28 -0300

    Set up IUrlRedirectRuleRepository

---
+++ demo/src/main/java/com/write/api/ports/out/repository/IUrlRedirectRuleRepository.java
@@ -0,0 +1,7 @@
+package com.write.api.ports.out.repository;
+
+import com.write.api.core.domain.model.UrlRedirectRuleModel;
+import com.write.api.ports.out.repository.shared.CrudRepository;
+
+public interface IUrlRedirectRuleRepository extends CrudRepository<UrlRedirectRuleModel, Long> {
+}
