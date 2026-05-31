7df3ccde1d99874f646129ad3cf3e42286745486
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-31 15:40:01 -0300

    Set up CreateUrlAccessRuleDTO

---
+++ demo/src/main/java/com/write/api/application/dto/urlAccessRule/CreateUrlAccessRuleDTO.java
@@ -0,0 +1,24 @@
+package com.write.api.application.dto.urlAccessRule;
+
+import com.write.api.core.domain.enums.UrlAccessRuleTypeEnum;
+import com.write.api.shared.validation.snowflake.IsId;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDateTime;
+
+public record CreateUrlAccessRuleDTO(
+
+        @IsId
+        Long urlId,
+
+        @NotNull
+        UrlAccessRuleTypeEnum type,
+
+        @NotBlank
+        String value,
+
+        LocalDateTime expiresAt
+
+) {
+}
