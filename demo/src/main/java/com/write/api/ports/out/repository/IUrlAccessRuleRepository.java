9315841057db695a0d659c74c6c9a87633392ed4
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-31 18:53:04 -0300

    Set up IUrlAccessRuleRepository

---
+++ demo/src/main/java/com/write/api/ports/out/repository/IUrlAccessRuleRepository.java
@@ -0,0 +1,8 @@
+package com.write.api.ports.out.repository;
+
+import com.write.api.core.domain.model.UrlAccessRuleModel;
+import com.write.api.ports.out.repository.shared.CrudRepository;
+
+public interface IUrlAccessRuleRepository extends CrudRepository<UrlAccessRuleModel, Long> {
+
+}
