8cef4c383296682deb40e8b21a194aa0a4e1ffc7
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-05-31 19:43:51 -0300

    Added annotation Getter and Setter

--- demo/src/main/java/com/write/api/core/domain/model/ApiKeyModel.java
+++ demo/src/main/java/com/write/api/core/domain/model/ApiKeyModel.java
@@ -1,16 +1,18 @@
 package com.write.api.core.domain.model;
 
+import com.write.api.core.domain.model.shared.BaseModel;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
-public class ApyKeyModel {
-    private Long id;
-    private Long version;
+@Getter
+@Setter
+public class ApiKeyModel extends BaseModel {
     private Long userId;
     private String keyHash;
     private String name;
     private boolean active;
     private LocalDateTime lastUsedAt;
     private LocalDateTime expiresAt;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
 }
