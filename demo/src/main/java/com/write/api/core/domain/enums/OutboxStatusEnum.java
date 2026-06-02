d3cc581ae3ea46bcae222b5b3be17ab1509b511c
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-02 19:38:01 -0300

    Set up OutboxStatusEnum

---
+++ demo/src/main/java/com/write/api/core/domain/enums/OutboxStatusEnum.java
@@ -0,0 +1,16 @@
+package com.write.api.core.domain.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum OutboxStatusEnum {
+
+    PENDING("Pendente"),
+    PROCESSING("Processando"),
+    PROCESSED("Processado"),
+    FAILED("Falhou");
+
+    private final String description;
+}
