0981fe0b7d501b4f954a43be508d4639bcd1895c
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-02 19:38:50 -0300

    Added Getter and Setter and new columns

--- demo/src/main/java/com/write/api/core/domain/model/OutboxEventModel.java
+++ demo/src/main/java/com/write/api/core/domain/model/OutboxEventModel.java
@@ -1,15 +1,36 @@
 package com.write.api.core.domain.model;
 
+import com.write.api.core.domain.enums.AggregateTypeEnum;
+import com.write.api.core.domain.enums.EventTypeEnum;
+import com.write.api.core.domain.enums.OutboxStatusEnum;
+import com.write.api.core.domain.enums.TopicEnum;
+import com.write.api.core.domain.model.shared.BaseModel;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
-public class OutboxEventModel {
+@Getter
+@Setter
+public class OutboxEventModel extends BaseModel {
 
-    private Long id;
-    private String aggregateType;
+    private AggregateTypeEnum aggregateType;
+
     private Long aggregateId;
-    private String eventType;
+
+    private EventTypeEnum eventType;
+
     private String payload;
-    private String status;
+
+    private TopicEnum topic;
+
+    private OutboxStatusEnum status;
+
+    private Integer retryCount;
+
+    private String errorMessage;
+
+    private LocalDateTime nextRetryAt;
+
     private LocalDateTime processedAt;
-    private LocalDateTime createdAt;
 }
