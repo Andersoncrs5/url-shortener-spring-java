c760cd1b36e99aff3f34bb631c230140801e4659
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-02 19:36:27 -0300

    Set up TopicEnum

---
+++ demo/src/main/java/com/write/api/core/domain/enums/TopicEnum.java
@@ -0,0 +1,20 @@
+package com.write.api.core.domain.enums;
+
+public enum TopicEnum {
+
+    USER_CREATED("user.created"),
+    USER_BLOCKED("user.blocked"),
+    USER_LOGIN_SUCCESS("user.login_success"),
+    USER_LOGIN_FAILED("user.login_failed"),
+    USER_DELETED("user.deleted");
+
+    private final String value;
+
+    TopicEnum(String value) {
+        this.value = value;
+    }
+
+    public String value() {
+        return value;
+    }
+}
