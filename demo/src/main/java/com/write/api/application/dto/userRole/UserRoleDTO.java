22d19c38ffa0a3c9b653bb9a9a69e07031d0ccd9
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-03 18:24:43 -0300

    Improvement in config

--- demo/src/main/java/com/write/api/application/dto/userRole/UserRoleDTO.java
+++ demo/src/main/java/com/write/api/application/dto/userRole/UserRoleDTO.java
@@ -1,15 +1,21 @@
 package com.write.api.application.dto.userRole;
 
+import com.write.api.shared.validation.snowflake.IsId;
+
 import java.time.LocalDateTime;
 
 public record UserRoleDTO(
 
+        @IsId
         Long id,
 
+        @IsId
         Long userId,
 
+        @IsId
         Long roleId,
 
+        @IsId
         Long assignedByUserId,
 
         LocalDateTime createdAt,
--- demo/src/main/java/com/write/api/infrastructure/config/kafka/KafkaConfig.java
+++ demo/src/main/java/com/write/api/infrastructure/config/kafka/KafkaConfig.java
@@ -16,19 +16,30 @@ import java.util.Map;
 public class KafkaConfig {
 
     @Bean
-    public ProducerFactory<String, Object> producerFactory() {
+    public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> config = new HashMap<>();
 
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        config.put(
+                ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
+                "localhost:9092"
+        );
+
+        config.put(
+                ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class
+        );
+
+        config.put(
+                ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                StringSerializer.class
+        );
 
         return new DefaultKafkaProducerFactory<>(config);
     }
 
     @Bean
-    public KafkaTemplate<String, Object> kafkaTemplate(
-            ProducerFactory<String, Object> factory
+    public KafkaTemplate<String, String> kafkaTemplate(
+            ProducerFactory<String, String> factory
     ) {
         return new KafkaTemplate<>(factory);
     }
