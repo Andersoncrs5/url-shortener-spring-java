7bb5ed082a5a2fafec41d974989ddc61013a8284
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-04 17:25:22 -0300

    New topic url.access.rule.created

--- demo/src/main/java/com/write/api/core/domain/enums/TopicEnum.java
+++ demo/src/main/java/com/write/api/core/domain/enums/TopicEnum.java
@@ -2,14 +2,17 @@ package com.write.api.core.domain.enums;
 
 public enum TopicEnum {
 
+    URL_ACCESS_RULE_CREATED("url.access.rule.created"),
+
     URL_CREATED("url.created"),
     URL_DELETED("url.deleted"),
+    URL_UPDATED("url.updated"),
 
     USER_CREATED("user.created"),
+    USER_DELETED("user.deleted"),
     USER_BLOCKED("user.blocked"),
     USER_LOGIN_SUCCESS("user.login_success"),
-    USER_LOGIN_FAILED("user.login_failed"),
-    USER_DELETED("user.deleted");
+    USER_LOGIN_FAILED("user.login_failed");
 
     private final String value;
 
@@ -20,4 +23,9 @@ public enum TopicEnum {
     public String value() {
         return value;
     }
+
+    public String dlq() {
+        return value + ".dlq";
+    }
 }
+
