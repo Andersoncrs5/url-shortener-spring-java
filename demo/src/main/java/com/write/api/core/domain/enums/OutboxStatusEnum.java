62618906dd020cd6cb106301cd3494ef81f83d1b
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-04 17:09:08 -0300

    New status RETRYING

--- demo/src/main/java/com/write/api/core/domain/enums/OutboxStatusEnum.java
+++ demo/src/main/java/com/write/api/core/domain/enums/OutboxStatusEnum.java
@@ -10,6 +10,7 @@ public enum OutboxStatusEnum {
     PENDING("Pendente"),
     PROCESSING("Processando"),
     PROCESSED("Processado"),
+    RETRYING("Retrying"),
     FAILED("Falhou");
 
     private final String description;
