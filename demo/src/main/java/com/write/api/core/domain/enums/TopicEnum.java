476e13aa745b8d2a2dd39b5adac8efe3e0931624
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-04 10:27:07 -0300

    New topic

--- demo/src/main/java/com/write/api/core/domain/enums/TopicEnum.java
+++ demo/src/main/java/com/write/api/core/domain/enums/TopicEnum.java
@@ -2,6 +2,9 @@ package com.write.api.core.domain.enums;
 
 public enum TopicEnum {
 
+    URL_CREATED("url.created"),
+    URL_DELETED("url.deleted"),
+
     USER_CREATED("user.created"),
     USER_BLOCKED("user.blocked"),
     USER_LOGIN_SUCCESS("user.login_success"),
