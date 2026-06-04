3476db2e6712f213224eeaa5c09be397652acd77
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-04 17:23:43 -0300

    Added annotation FieldDefaults

--- demo/src/main/java/com/write/api/core/domain/model/UserRoleModel.java
+++ demo/src/main/java/com/write/api/core/domain/model/UserRoleModel.java
@@ -1,13 +1,16 @@
 package com.write.api.core.domain.model;
 
 import com.write.api.core.domain.model.shared.BaseModel;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
 @Getter
 @Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserRoleModel extends BaseModel {
-    private Long userId;
-    private Long roleId;
-    private Long assignedByUserId;
+    Long userId;
+    Long roleId;
+    Long assignedByUserId;
 }
