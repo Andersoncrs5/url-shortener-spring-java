a1a58dda9c88a1665fde3ce019f07f21e23d555d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-04 17:25:36 -0300

    Added annotation FieldDefaults

--- demo/src/main/java/com/write/api/core/domain/model/RoleModel.java
+++ demo/src/main/java/com/write/api/core/domain/model/RoleModel.java
@@ -1,13 +1,16 @@
 package com.write.api.core.domain.model;
 
 import com.write.api.core.domain.model.shared.BaseModel;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
 @Getter
 @Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class RoleModel extends BaseModel {
-    private String name;
-    private String description;
-    private boolean active;
+    String name;
+    String description;
+    boolean active;
 }
