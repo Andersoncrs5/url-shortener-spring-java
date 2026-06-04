b148ac86e98f433b1146d7ab7a1b04dbaffae2b2
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-04 17:22:31 -0300

    Import EnumMapper

--- demo/src/main/java/com/write/api/shared/mapper/config/CentralMapperConfig.java
+++ demo/src/main/java/com/write/api/shared/mapper/config/CentralMapperConfig.java
@@ -9,7 +9,8 @@ import org.mapstruct.ReportingPolicy;
         uses = { MapperConfig.class },
         nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
         unmappedTargetPolicy = ReportingPolicy.IGNORE,
-        collectionMappingStrategy = CollectionMappingStrategy.ADDER_PREFERRED
+        collectionMappingStrategy = CollectionMappingStrategy.ADDER_PREFERRED,
+        imports = { EnumMapper.class }
 )
 public class CentralMapperConfig {
 }
