d5a16fbfbdf7231e575ebea78b39ca263db1c48e
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-04 17:24:04 -0300

    Added annotation FieldDefaults, getter, setter

--- demo/src/main/java/com/write/api/core/domain/model/UrlTagModel.java
+++ demo/src/main/java/com/write/api/core/domain/model/UrlTagModel.java
@@ -1,97 +1,22 @@
 package com.write.api.core.domain.model;
 
-import java.time.LocalDateTime;
+import com.write.api.core.domain.model.shared.BaseModel;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
-public class UrlTagModel {
+@Setter
+@Getter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UrlTagModel extends BaseModel {
 
-    private Long id;
-    private Long userId;
-    private String name;
-    private String slug;
-    private String color;
-    private String description;
-    private Long parentId;
-    private boolean active;
-    private LocalDateTime createdAt;
-    private LocalDateTime updatedAt;
+    Long userId;
+    String name;
+    String slug;
+    String color;
+    String description;
+    Long parentId;
+    boolean active;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSlug() {
-        return slug;
-    }
-
-    public void setSlug(String slug) {
-        this.slug = slug;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Long getParentId() {
-        return parentId;
-    }
-
-    public void setParentId(Long parentId) {
-        this.parentId = parentId;
-    }
-
-    public boolean isActive() {
-        return active;
-    }
-
-    public void setActive(boolean active) {
-        this.active = active;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 }
