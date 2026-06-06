c7348db6e42a4e15a27d704d84d00a6f548f4053
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-06 14:45:04 -0300

    .

--- demo/target/generated-sources/annotations/com/write/api/adapters/in/web/mapper/UrlAccessRuleMapperImpl.java
+++ demo/target/generated-sources/annotations/com/write/api/adapters/in/web/mapper/UrlAccessRuleMapperImpl.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2026-06-06T10:42:15-0300",
-    comments = "version: 1.6.3, compiler: javac, environment: Java 25.0.2 (GraalVM Community)"
+    date = "2026-06-06T14:38:01-0300",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 25.0.2 (Eclipse Adoptium)"
 )
 @Component
 public class UrlAccessRuleMapperImpl implements UrlAccessRuleMapper {
