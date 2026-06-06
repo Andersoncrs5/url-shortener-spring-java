fcb1bfcb23594dfb6dc48c8f50a633f16e31cccd
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-06 15:33:30 -0300

    New method findToDelete

--- demo/src/main/java/com/write/api/ports/out/repository/IUrlRepository.java
+++ demo/src/main/java/com/write/api/ports/out/repository/IUrlRepository.java
@@ -1,10 +1,15 @@
 package com.write.api.ports.out.repository;
 
+import com.write.api.core.domain.enums.UrlStatusEnum;
 import com.write.api.core.domain.model.UrlModel;
 import com.write.api.ports.out.repository.shared.CrudRepository;
 import com.write.api.shared.validation.snowflake.IsId;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface IUrlRepository extends CrudRepository<UrlModel, Long> {
     boolean existsByShortCode(String code);
     boolean existsByUserIdAndUrlId(@IsId Long userId, @IsId Long urlId);
+    List<UrlModel> findToDelete(UrlStatusEnum status, int limit, LocalDateTime deletedAt);
 }
