ae54be8a2f498609383f4e93c88460b267cec232
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-09 19:59:23 -0300

    Added annotation FieldDefaults

--- api/src/main/java/com/read/api/api/dto/base/BaseDTO.java
+++ api/src/main/java/com/read/api/api/dto/base/BaseDTO.java
@@ -1,12 +1,15 @@
 package com.read.api.api.dto.base;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class BaseDTO {
     Long id;
     LocalDateTime createdAt;
