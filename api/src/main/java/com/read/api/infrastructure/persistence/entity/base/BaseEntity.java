a344ae1a779e28649dd5fdd563de3b6d5eee5d19
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-09 14:52:30 -0300

    Set up Base to Entities

---
+++ api/src/main/java/com/read/api/infrastructure/persistence/entity/base/BaseEntity.java
@@ -0,0 +1,21 @@
+package com.read.api.infrastructure.persistence.entity.base;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public abstract class BaseEntity {
+
+    @Id
+    Long id;
+
+    LocalDateTime createdAt;
+    LocalDateTime updatedAt;
+}
