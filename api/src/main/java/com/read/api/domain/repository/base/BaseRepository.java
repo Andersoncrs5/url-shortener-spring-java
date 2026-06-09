b001f6ca862ea73538bc00e39a60b8824e7810aa
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-09 16:28:32 -0300

    Set up BaseRepository

---
+++ api/src/main/java/com/read/api/domain/repository/base/BaseRepository.java
@@ -0,0 +1,17 @@
+package com.read.api.domain.repository.base;
+
+import com.read.api.api.dto.base.BaseFilter;
+import com.read.api.domain.model.base.BaseModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+public interface BaseRepository<T extends BaseModel, ID, F extends BaseFilter> {
+    T save(T user);
+    T insert(T user);
+    Optional<T> findById(ID id);
+    boolean existsById(ID id);
+    int deleteById(ID id);
+    Page<T> findAll(F filter, Pageable pageable);
+}
