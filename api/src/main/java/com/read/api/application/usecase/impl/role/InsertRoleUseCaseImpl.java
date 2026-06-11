21cd1b2bb90f531fc8116becbcaf5f712387d7ee
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-11 15:05:40 -0300

    Set up Logic, use case and test to InsertRole

---
+++ api/src/main/java/com/read/api/application/usecase/impl/role/InsertRoleUseCaseImpl.java
@@ -0,0 +1,26 @@
+package com.read.api.application.usecase.impl.role;
+
+import com.read.api.application.usecase.interfaces.role.InsertRoleUseCase;
+import com.read.api.domain.model.RoleModel;
+import com.read.api.domain.repository.RoleRepository;
+import com.read.api.utils.result.Result;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@Service
+@Validated
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class InsertRoleUseCaseImpl implements InsertRoleUseCase {
+    RoleRepository repository;
+
+    @Override
+    public Result<RoleModel> execute(RoleModel role) {
+        RoleModel saved = repository.insert(role);
+
+        return Result.success(saved);
+    }
+}
