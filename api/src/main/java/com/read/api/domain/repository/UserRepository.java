9d174ba7d138c59dced92d4726421d994a107ce9
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-15 15:58:14 -0300

    New method existsByNameIgnoreCase

--- api/src/main/java/com/read/api/domain/repository/UserRepository.java
+++ api/src/main/java/com/read/api/domain/repository/UserRepository.java
@@ -9,4 +9,5 @@ import java.util.Optional;
 public interface UserRepository extends BaseRepository<UserModel, Long, UserFilter> {
     Optional<UserModel> findByEmailIgnoreCase(String email);
     boolean existsByEmailIgnoreCase(String email);
+    boolean existsByNameIgnoreCase(String name);
 }
