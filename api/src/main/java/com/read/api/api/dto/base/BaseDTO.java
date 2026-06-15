34ec480d81d295b70d16984f6239b9996472074b
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-15 16:02:14 -0300

    Added column version

--- api/src/main/java/com/read/api/api/dto/base/BaseDTO.java
+++ api/src/main/java/com/read/api/api/dto/base/BaseDTO.java
@@ -12,6 +12,7 @@ import java.time.LocalDateTime;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class BaseDTO {
     Long id;
+    Long version;
     LocalDateTime createdAt;
     LocalDateTime updatedAt;
 }
