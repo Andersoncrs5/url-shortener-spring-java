5c4cd75334a9bc002df8cb4edbcbb50c2c44bc83
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-16 15:45:22 -0300

    New method createUrlAccessRule

--- api/src/test/java/com/read/api/application/usecase/base/BaseUseCaseTest.java
+++ api/src/test/java/com/read/api/application/usecase/base/BaseUseCaseTest.java
@@ -1,11 +1,14 @@
 package com.read.api.application.usecase.base;
 
+import com.read.api.domain.model.UrlAccessRuleModel;
 import com.read.api.domain.model.UserModel;
 import com.read.api.domain.service.RedisCrudService;
 import com.read.api.domain.utils.SnowflakeIdGenerator;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
 
 @ExtendWith(MockitoExtension.class)
 public abstract class BaseUseCaseTest {
@@ -24,6 +27,17 @@ public abstract class BaseUseCaseTest {
         user.setEmail("pochita@gmail.com");
 
         return user;
+    }
+
+    public UrlAccessRuleModel createUrlAccessRule() {
+        var rule = new UrlAccessRuleModel();
+
+        rule.setId(generator.nextId());
+        rule.setUrlId(generator.nextId());
+        rule.setAssignedByUserId(generator.nextId());
+        rule.setExpiresAt(LocalDateTime.now().plusDays(55));
+
+        return rule;
     }
 
 }
