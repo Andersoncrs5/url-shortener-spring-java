2b899e84418bb28148b93129602bec53c1630624
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-16 16:29:31 -0300

    Set up BaseIntegrationTest

---
+++ api/src/test/java/com/read/api/api/base/BaseIntegrationTest.java
@@ -0,0 +1,152 @@
+package com.read.api.api.base;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.read.api.TestcontainersConfiguration;
+import com.read.api.domain.enums.UrlAccessRuleTypeEnum;
+import com.read.api.domain.model.UrlAccessRuleModel;
+import com.read.api.domain.model.UserModel;
+import com.read.api.domain.repository.UrlAccessRuleRepository;
+import com.read.api.domain.repository.UserRepository;
+import com.read.api.domain.utils.Base62;
+import com.read.api.domain.utils.SnowflakeIdGenerator;
+import com.read.api.infrastructure.config.security.TokenService;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@FieldDefaults(level = AccessLevel.PROTECTED)
+@Import(TestcontainersConfiguration.class)
+public class BaseIntegrationTest {
+
+    @Autowired SnowflakeIdGenerator generator;
+    @Autowired MockMvc mockMvc;
+    @Autowired ObjectMapper objectMapper;
+    @Autowired TokenService tokenService;
+
+    @Autowired UserRepository userRepository;
+    @Autowired UrlAccessRuleRepository urlAccessRuleRepository;
+
+    protected String createUser() {
+
+        UserModel user = new UserModel();
+
+        user.setId(generator.nextId());
+        user.setName("Pochita "+ this.generateRandomChars(50).toLowerCase() );
+        user.setEmail("pochita" + this.generateRandomChars(51).toLowerCase() + "@gmail.com");
+        user.setActive(true);
+        user.setVersion(1L);
+        user.setCreatedAt(LocalDateTime.now());
+        user.setUpdatedAt(LocalDateTime.now());
+
+        UserModel inserted = userRepository.insert(user);
+
+        return tokenService.generateToken(inserted);
+    }
+
+    protected UserModel createUser(
+            String name,
+            String email
+    ) {
+
+        UserModel user = new UserModel();
+
+        user.setId(generator.nextId());
+        user.setName(name);
+        user.setEmail(email);
+        user.setActive(true);
+        user.setVersion(1L);
+
+        LocalDateTime now = LocalDateTime.now();
+
+        user.setCreatedAt(now);
+        user.setUpdatedAt(now);
+
+        return userRepository.insert(user);
+    }
+
+    protected UserModel createUserFast() {
+
+        UserModel user = new UserModel();
+
+        user.setId(generator.nextId());
+        user.setName("Pochita "+ this.generateRandomChars(50).toLowerCase() );
+        user.setEmail("pochita" + this.generateRandomChars(51).toLowerCase() + "@gmail.com");
+        user.setActive(true);
+        user.setVersion(1L);
+
+        LocalDateTime now = LocalDateTime.now();
+
+        user.setCreatedAt(now);
+        user.setUpdatedAt(now);
+
+        return userRepository.insert(user);
+    }
+
+    protected UrlAccessRuleModel createUrlAccessRule() {
+        UrlAccessRuleModel rule = new UrlAccessRuleModel();
+        var now = LocalDateTime.now();
+
+        rule.setId(generator.nextId());
+        rule.setAssignedByUserId(generator.nextId());
+        rule.setUrlId(generator.nextId());
+        rule.setExpiresAt(LocalDateTime.now().plusDays(11));
+        rule.setActive(true);
+        rule.setRuleValue("11");
+        rule.setType(UrlAccessRuleTypeEnum.RATE_LIMIT);
+        rule.setCreatedAt(now);
+        rule.setUpdatedAt(now);
+
+        return urlAccessRuleRepository.insert(rule);
+    }
+
+    protected ArrayList<UrlAccessRuleModel> createManyUrlAccessRule(int amount) {
+        ArrayList<UrlAccessRuleModel> list = new ArrayList<>();
+
+        for (int i = 0; i < amount; i++) {
+            UrlAccessRuleModel rule = this.createUrlAccessRule();
+
+            list.add(rule);
+        }
+
+        return list;
+    }
+
+    protected ArrayList<UserModel> createManyUsers(int amount) {
+        ArrayList<UserModel> urs = new ArrayList<>();
+        for (int i = 0; i < amount; i++) {
+
+            UserModel user = createUser(
+                    "user-" + i,
+                    "user-" + i + "@gmail.com"
+            );
+
+            urs.add(user);
+        }
+
+        return urs;
+    }
+
+    protected String generateRandomChars(int size) {
+        StringBuilder builder = new StringBuilder();
+
+        while (builder.length() < size) {
+            builder.append(
+                    Base62.encode(generator.nextId())
+            );
+        }
+
+        return builder.substring(0, size);
+    }
+}
