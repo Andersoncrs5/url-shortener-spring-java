ae7174b01b68f84629b08a32070372ffe8b5b920
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-16 16:28:27 -0300

    New methods findByEmailIgnoreCase, existsByEmailIgnoreCase, existsByNameIgnoreCase

--- api/src/main/java/com/read/api/infrastructure/persistence/mongo/MongoUserRepository.java
+++ api/src/main/java/com/read/api/infrastructure/persistence/mongo/MongoUserRepository.java
@@ -1,7 +1,13 @@
 package com.read.api.infrastructure.persistence.mongo;
 
+import com.read.api.domain.model.UserModel;
 import com.read.api.infrastructure.persistence.entity.UserEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface MongoUserRepository extends MongoRepository<UserEntity, Long> {
+    Optional<UserModel> findByEmailIgnoreCase(String email);
+    boolean existsByEmailIgnoreCase(String email);
+    boolean existsByNameIgnoreCase(String email);
 }
