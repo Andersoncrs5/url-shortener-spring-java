28a481eb986867663c108ec5c34673799971476d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-17 15:55:12 -0300

    Set up UrlDTO

---
+++ api/src/main/java/com/read/api/api/dto/url/UrlDTO.java
@@ -0,0 +1,33 @@
+package com.read.api.api.dto.url;
+
+import com.read.api.api.dto.base.BaseDTO;
+import com.read.api.domain.enums.UrlAccessTypeEnum;
+import com.read.api.domain.enums.UrlStatusEnum;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.FieldDefaults;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class UrlDTO extends BaseDTO {
+
+    Long userId;
+    String shortCode;
+    String description;
+    String faviconUrl;
+    String originalUrl;
+    String title;
+    String domain;
+    UrlStatusEnum status;
+    UrlAccessTypeEnum accessType;
+    String passwordHash;
+    boolean customAlias;
+    LocalDateTime deletedAt;
+    LocalDateTime expiresAt;
+    LocalDateTime lastAccessAt;
+
+}
