c4ccdcaea3ae3fddb4c8ce7e940f298ec41bdce1
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-17 14:22:35 -0300

    Added annotation FieldDefaults

--- api/src/main/java/com/read/api/api/dto/urlAccessRule/UrlAccessRuleFilter.java
+++ api/src/main/java/com/read/api/api/dto/urlAccessRule/UrlAccessRuleFilter.java
@@ -2,13 +2,16 @@ package com.read.api.api.dto.urlAccessRule;
 
 import com.read.api.api.dto.base.BaseFilter;
 import com.read.api.domain.enums.UrlAccessRuleTypeEnum;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.experimental.FieldDefaults;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class UrlAccessRuleFilter extends BaseFilter {
     Long urlId;
 
