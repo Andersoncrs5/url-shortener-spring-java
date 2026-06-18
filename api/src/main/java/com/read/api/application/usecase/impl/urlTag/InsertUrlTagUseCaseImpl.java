7dd62b4a3a13d9d909859c0024a0f228d5a280ce
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-18 15:14:27 -0300

    Impled logic, useCase and test to InsertUrlTag

---
+++ api/src/main/java/com/read/api/application/usecase/impl/urlTag/InsertUrlTagUseCaseImpl.java
@@ -0,0 +1,27 @@
+package com.read.api.application.usecase.impl.urlTag;
+
+import com.read.api.application.usecase.interfaces.urlTag.InsertUrlTagUseCase;
+import com.read.api.application.usecase.interfaces.urlTag.SaveUrlTagUseCase;
+import com.read.api.domain.model.UrlTagModel;
+import com.read.api.domain.repository.UrlTagRepository;
+import com.read.api.utils.result.Result;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.stereotype.Service;
+import org.springframework.validation.annotation.Validated;
+
+@Service
+@Validated
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class InsertUrlTagUseCaseImpl implements InsertUrlTagUseCase {
+    UrlTagRepository repository;
+
+    @Override
+    public Result<UrlTagModel> execute(UrlTagModel model) {
+        UrlTagModel save = repository.insert(model);
+
+        return Result.success(save, 200);
+    }
+}
---
+++ api/src/main/java/com/read/api/application/usecase/interfaces/urlTag/InsertUrlTagUseCase.java
@@ -0,0 +1,8 @@
+package com.read.api.application.usecase.interfaces.urlTag;
+
+import com.read.api.domain.model.UrlTagModel;
+import com.read.api.utils.result.Result;
+
+public interface InsertUrlTagUseCase {
+    Result<UrlTagModel> execute(UrlTagModel model);
+}
