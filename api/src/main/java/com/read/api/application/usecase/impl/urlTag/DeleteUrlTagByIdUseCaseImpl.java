29a2028c54ac7daba775f6ff42e4c49298bdd613
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-18 19:24:53 -0300

    Added annotation UseCase

--- api/src/main/java/com/read/api/application/usecase/impl/urlTag/DeleteUrlTagByIdUseCaseImpl.java
+++ api/src/main/java/com/read/api/application/usecase/impl/urlTag/DeleteUrlTagByIdUseCaseImpl.java
@@ -1,16 +1,14 @@
 package com.read.api.application.usecase.impl.urlTag;
 
+import com.read.api.application.usecase.base.UseCase;
 import com.read.api.application.usecase.interfaces.urlTag.DeleteUrlTagByIdUseCase;
 import com.read.api.domain.repository.UrlTagRepository;
 import com.read.api.utils.result.Result;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.stereotype.Service;
-import org.springframework.validation.annotation.Validated;
 
-@Service
-@Validated
+@UseCase
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class DeleteUrlTagByIdUseCaseImpl implements DeleteUrlTagByIdUseCase {
