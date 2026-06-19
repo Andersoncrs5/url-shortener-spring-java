22402789927887ec5519c84ed830b19c23983da6
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-19 14:41:21 -0300

    Added CentralMapperConfig

--- api/src/main/java/com/read/api/api/controller/urlAccessRule/UrlAccessRuleMapperController.java
+++ api/src/main/java/com/read/api/api/controller/urlAccessRule/UrlAccessRuleMapperController.java
@@ -2,9 +2,10 @@ package com.read.api.api.controller.urlAccessRule;
 
 import com.read.api.api.dto.urlAccessRule.UrlAccessRuleDTO;
 import com.read.api.domain.model.UrlAccessRuleModel;
+import com.read.api.infrastructure.mapper.CentralMapperConfig;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", config = CentralMapperConfig.class)
 public interface UrlAccessRuleMapperController {
     UrlAccessRuleDTO toDTO(UrlAccessRuleModel model);
 }
