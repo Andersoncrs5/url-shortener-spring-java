169974d5d8998c067e456074219b6a00d3548868
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-19 14:41:15 -0300

    Added CentralMapperConfig

--- api/src/main/java/com/read/api/infrastructure/persistence/mapper/OutboxMapperRepository.java
+++ api/src/main/java/com/read/api/infrastructure/persistence/mapper/OutboxMapperRepository.java
@@ -1,10 +1,11 @@
 package com.read.api.infrastructure.persistence.mapper;
 
 import com.read.api.domain.model.OutboxEventModel;
+import com.read.api.infrastructure.mapper.CentralMapperConfig;
 import com.read.api.infrastructure.persistence.entity.OutboxEventEntity;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+@Mapper(config = CentralMapperConfig.class)
 public interface OutboxMapperRepository {
 
     OutboxEventModel toModel(OutboxEventEntity entity);
