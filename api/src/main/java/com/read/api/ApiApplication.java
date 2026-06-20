478b9b8b39f747c86e06e443edaba3e393310fa6
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-20 14:42:28 -0300

    Added annotation EnableCaching

--- api/src/main/java/com/read/api/ApiApplication.java
+++ api/src/main/java/com/read/api/ApiApplication.java
@@ -3,9 +3,11 @@ package com.read.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableCaching
 public class ApiApplication {
 
 	public static void main(String[] args) {
