cffe1f925409bb6b1dde31b9f57bef8a9d8942a5
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-06-27 11:14:29 -0300

    Improvements add cache

--- api/src/main/java/com/read/api/infrastructure/config/security/CustomUserDetailsService.java
+++ api/src/main/java/com/read/api/infrastructure/config/security/CustomUserDetailsService.java
@@ -2,37 +2,78 @@ package com.read.api.infrastructure.config.security;
 
 import com.read.api.domain.model.UserModel;
 import com.read.api.domain.repository.UserRepository;
+import com.read.api.domain.service.RedisCrudService;
+import com.read.api.utils.metrics.observed.ObservedMetric;
+import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.jspecify.annotations.NonNull;
 import com.read.api.infrastructure.config.security.classes.UserPrincipal;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class CustomUserDetailsService implements UserDetailsService {
-    private final UserRepository userRepository;
+    UserRepository userRepository;
+    RedisCrudService redis;
 
     @Override
-    public @NonNull UserDetails loadUserByUsername(@NonNull String email) throws UsernameNotFoundException {
-        UserModel user = userRepository.findByEmailIgnoreCase(email)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    @ObservedMetric("custom.user.details.load.user.username")
+    public @NonNull UserDetails loadUserByUsername(
+            @NonNull String email
+    ) throws UsernameNotFoundException {
+        String key = "auth:" + email;
 
-        var roles = user.getRoles();
+        UserModel user = redis.find(
+                key,
+                UserModel.class
+        ).orElseGet(() -> {
+
+            UserModel loaded =
+                    userRepository
+                            .findByEmailIgnoreCase(email)
+                            .orElseThrow(() ->
+                                    new UsernameNotFoundException(
+                                            "User not found"
+                                    ));
+
+            redis.save(key, loaded);
+
+            return loaded;
+        });
+
+        if (!user.isActive()) {
+            throw new DisabledException("User disabled");
+        }
+
+        if (user.getBlockedAt() != null) {
+            throw new LockedException("User blocked");
+        }
 
         return new UserPrincipal(
                 user.getId(),
                 user.getEmail(),
-                "null",
-                roles.stream()
-                        .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                "password",
+                user.getRoles()
+                        .stream()
+                        .map(role ->
+                                new SimpleGrantedAuthority(
+                                        "ROLE_" + role
+                                ))
                         .toList(),
                 user
         );
     }
+
 }
