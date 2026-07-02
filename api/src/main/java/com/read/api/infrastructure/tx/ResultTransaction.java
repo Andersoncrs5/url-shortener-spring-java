fd307b865f4f79e94b06613ba56c4228018e03d9
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-07-02 14:17:21 -0300

    Set up ResultTransaction

---
+++ api/src/main/java/com/read/api/infrastructure/tx/ResultTransaction.java
@@ -0,0 +1,25 @@
+package com.read.api.infrastructure.tx;
+
+import org.springframework.transaction.TransactionDefinition;
+
+import org.springframework.transaction.annotation.Isolation;
+
+import org.springframework.transaction.annotation.Propagation;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ResultTransaction {
+    boolean readOnly() default false;
+
+    int timeout() default TransactionDefinition.TIMEOUT_DEFAULT;
+
+    Propagation propagation() default Propagation.REQUIRED;
+
+    Isolation isolation() default Isolation.DEFAULT;
+
+}
---
+++ api/src/main/java/com/read/api/infrastructure/tx/ResultTransactionAspect.java
@@ -0,0 +1,64 @@
+package com.read.api.infrastructure.tx;
+
+import com.read.api.utils.result.Result;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+@Aspect
+@Component
+@RequiredArgsConstructor
+public class ResultTransactionAspect {
+
+    private final MongoTransactionManager transactionManager;
+
+    @Around("@annotation(resultTransaction)")
+    public Object around(
+            ProceedingJoinPoint joinPoint,
+            ResultTransaction resultTransaction
+    ) throws Throwable {
+
+        DefaultTransactionDefinition definition = new DefaultTransactionDefinition();
+
+        definition.setReadOnly(resultTransaction.readOnly());
+        definition.setPropagationBehavior(resultTransaction.propagation().value());
+        definition.setIsolationLevel(resultTransaction.isolation().value());
+        definition.setTimeout(resultTransaction.timeout());
+
+        TransactionStatus status = transactionManager.getTransaction(definition);
+
+        try {
+            Object returned = joinPoint.proceed();
+
+            if (returned instanceof Result<?> result && result.isFailure()) {
+
+                if (status.isNewTransaction()) {
+                    transactionManager.rollback(status);
+                } else {
+                    status.setRollbackOnly();
+                }
+
+                return returned;
+            }
+
+            if (status.isNewTransaction()) {
+                transactionManager.commit(status);
+            }
+
+            return returned;
+
+        } catch (Throwable ex) {
+            if (status.isNewTransaction()) {
+                transactionManager.rollback(status);
+            } else {
+                status.setRollbackOnly();
+            }
+            throw ex;
+        }
+    }
+}
