4e2e757b206065fdb8dc2323c0b605a70c9acb7d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-07-02 14:19:33 -0300

    Set up DeadLetterEventPageRequestDTO

---
+++ api/src/main/java/com/read/api/api/controller/deadLetterEvent/DeadLetterEventPageRequestDTO.java
@@ -0,0 +1,27 @@
+package com.read.api.api.controller.deadLetterEvent;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+@Setter
+@Getter
+public class DeadLetterEventPageRequestDTO {
+    private Integer page = 0;
+
+    private Integer size = 30;
+
+    private DeadLetterEventOrderBy orderBy = DeadLetterEventOrderBy.CREATED_AT;
+
+    private Sort.Direction direction = Sort.Direction.DESC;
+
+    public Pageable toPageable() {
+        return PageRequest.of(
+                page,
+                size,
+                Sort.by(direction, orderBy.getField())
+        );
+    }
+}
