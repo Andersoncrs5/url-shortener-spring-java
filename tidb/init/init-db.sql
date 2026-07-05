7a26a8423c244be31acb826160f103aa282201b6
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-07-05 18:09:46 -0300

    Connected the grafana in tidb

--- tidb/init/init-db.sql
+++ tidb/init/init-db.sql
@@ -6,6 +6,7 @@ CREATE DATABASE IF NOT EXISTS url_shortener_test
   CHARACTER SET utf8mb4
   COLLATE utf8mb4_bin;
 
+-- Banco interno do Grafana
 CREATE DATABASE IF NOT EXISTS grafana
   CHARACTER SET utf8mb4
   COLLATE utf8mb4_bin;
@@ -13,7 +14,8 @@ CREATE DATABASE IF NOT EXISTS grafana
 CREATE USER IF NOT EXISTS 'grafana'@'%' IDENTIFIED BY 'grafana';
 
 GRANT SELECT ON url_shortener.* TO 'grafana'@'%';
-
 GRANT SELECT ON information_schema.* TO 'grafana'@'%';
+
+GRANT ALL PRIVILEGES ON grafana.* TO 'grafana'@'%';
 
 FLUSH PRIVILEGES;
