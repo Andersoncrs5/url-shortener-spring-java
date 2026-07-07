55f1697db08caaee14b80e5a03dbfe65fc74daa5
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-07-07 20:05:54 -0300

    Added Set up RedisCrudService

---
+++ notify/src/main/java/com/notify/notify/globals/services/redis/RedisCrudService.java
@@ -0,0 +1,32 @@
+package com.notify.notify.globals.services.redis;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import java.time.Duration;
+import java.util.Collection;
+import java.util.Optional;
+
+public interface RedisCrudService {
+
+    long increment(String key);
+
+    long increment(String key, long delta);
+
+    <T> void save(String key, T value, Duration ttl);
+
+    <T> void save(String key, T value);
+
+    <T> Optional<T> find(String key, Class<T> type);
+
+    <T> Optional<T> find(String key, TypeReference<T> typeReference);
+
+    boolean exists(String key);
+
+    void delete(String key);
+
+    long delete(Collection<String> keys);
+
+    boolean saveIfAbsent(String key, Object value, Duration ttl);
+
+    boolean expire(String key, Duration ttl);
+}
