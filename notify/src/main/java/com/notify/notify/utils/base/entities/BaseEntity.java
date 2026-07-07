b9edc97c30b0700dd7378ca6a70eae1af6495f33
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-07-07 20:04:33 -0300

    Set up BaseEntity

---
+++ notify/src/main/java/com/notify/notify/utils/base/entities/BaseEntity.java
@@ -0,0 +1,35 @@
+package com.notify.notify.utils.base.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
