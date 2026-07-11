f524bc5ab033e071dfda645fa4182ea6009e5012
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-07-11 10:25:15 -0300

    Added annotations and DSLContext

--- demo/src/test/java/com/write/api/adapters/in/web/controller/BaseControllerTest.java
+++ demo/src/test/java/com/write/api/adapters/in/web/controller/BaseControllerTest.java
@@ -1,15 +1,26 @@
 package com.write.api.adapters.in.web.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.write.api.TestcontainersConfiguration;
 import com.write.api.adapters.in.web.controller.util.helps.HelperTest;
 import com.write.api.core.domain.service.SnowflakeIdGenerator;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
+import org.jooq.DSLContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
 @FieldDefaults(level = AccessLevel.PROTECTED)
+@Import(TestcontainersConfiguration.class)
 public abstract class BaseControllerTest {
+    @Autowired DSLContext dsl;
     @Autowired MockMvc mockMvc;
     @Autowired ObjectMapper objectMapper;
     @Autowired SnowflakeIdGenerator idGen;
