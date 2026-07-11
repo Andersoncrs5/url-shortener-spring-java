acf6da6fe921d9447b60690d0f5a962722e5b954
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-07-11 17:07:13 -0300

    Set up DataSourceConfigProperties

---
+++ notify/src/main/java/com/notify/notify/configs/properties/DataSourceConfigProperties.java
@@ -0,0 +1,17 @@
+package com.notify.notify.configs.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "spring.datasource")
+public class DataSourceConfigProperties {
+
+    private String url;
+    private String username;
+    private String password;
+    private String driverClassName;
+
+}
