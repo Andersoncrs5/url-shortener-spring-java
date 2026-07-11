a905dd35bee0972fc725cc6136e9e4d994e7e560
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-07-11 17:11:25 -0300

    Set up Mapppers

---
+++ notify/src/main/java/com/notify/notify/configs/mapper/MapperConfig.java
@@ -0,0 +1,18 @@
+package com.notify.notify.configs.mapper;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MapperConfig {
+
+    public boolean map(Byte value) {
+        return value != null && value == 1;
+    }
+
+    public Byte map(boolean value) {
+        return (byte) (value ? 1 : 0);
+    }
+
+
+
+}
