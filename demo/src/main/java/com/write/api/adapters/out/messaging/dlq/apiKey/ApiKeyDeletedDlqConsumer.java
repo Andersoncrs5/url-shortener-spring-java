741ce7adc489e024ed22548384f9a6c391dc817c
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-07-11 10:24:40 -0300

    Added ApiKeyDeletedDlqConsumer

---
+++ demo/src/main/java/com/write/api/adapters/out/messaging/dlq/apiKey/ApiKeyDeletedDlqConsumer.java
@@ -0,0 +1,45 @@
+package com.write.api.adapters.out.messaging.dlq.apiKey;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.write.api.application.dto.messaging.OutboxEventMessage;
+import com.write.api.application.dto.outbox.events.apiKey.ApiKeyDeletedEvent;
+import com.write.api.application.shared.Result;
+import com.write.api.ports.in.outbox.HandlerDlqUseCase;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class ApiKeyDeletedDlqConsumer {
+    HandlerDlqUseCase handlerDlq;
+
+    @KafkaListener(
+            topics = "api.key.deleted.dlq",
+            groupId = "url-shortener-dlq"
+    )
+    public void consume(String payload) {
+
+        Result<Void> result = handlerDlq.execute(
+                payload,
+                new TypeReference<OutboxEventMessage<ApiKeyDeletedEvent>>() {}
+        );
+
+        if (result.isFailure()) {
+            log.error(
+                    "Failed to reprocess DLQ event. Status: {}, Message: {}",
+                    result.getStatusCode(),
+                    result.getMessage()
+            );
+            return;
+        }
+
+        log.info("DLQ event successfully reprocessed");
+    }
+
+}
