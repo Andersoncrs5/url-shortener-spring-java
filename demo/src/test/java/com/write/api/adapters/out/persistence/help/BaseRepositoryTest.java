2f7ce99fc9aa42ce59f04e640d170e5cd4048ea7
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-07-11 10:25:31 -0300

    Added annotations SpringBootTest, AutoConfigureMockMvc, ActiveProfiles

--- demo/src/test/java/com/write/api/adapters/out/persistence/help/BaseRepositoryTest.java
+++ demo/src/test/java/com/write/api/adapters/out/persistence/help/BaseRepositoryTest.java
@@ -1,16 +1,20 @@
 package com.write.api.adapters.out.persistence.help;
 
+import com.write.api.TestcontainersConfiguration;
 import com.write.api.core.domain.service.SnowflakeIdGenerator;
 import org.jooq.DSLContext;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
 
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+@Import(TestcontainersConfiguration.class)
 public abstract class BaseRepositoryTest {
-    @Autowired
-    protected HelpRepositoryTest help;
-
-    @Autowired
-    protected SnowflakeIdGenerator generator;
-
-    @Autowired
-    protected DSLContext dsl;
+    @Autowired protected HelpRepositoryTest help;
+    @Autowired protected SnowflakeIdGenerator generator;
+    @Autowired protected DSLContext dsl;
 }
