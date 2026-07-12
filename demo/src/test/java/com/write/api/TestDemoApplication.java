4a2ced0fb3511f62cb03da179b7e98ad70fd41e1
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-07-12 13:15:15 -0300

    New test

--- demo/src/test/java/com/write/api/TestDemoApplication.java
+++ demo/src/test/java/com/write/api/TestDemoApplication.java
@@ -1,12 +1,24 @@
 package com.write.api;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringApplication;
+import org.testcontainers.containers.MySQLContainer;
 
 public class TestDemoApplication {
 
 	static void main(String[] args) {
-		SpringApplication.from(DemoApplication::main);
-//				.with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.from(DemoApplication::main)
+				.with(TestcontainersConfiguration.class).run(args);
+	}
+
+	@Test
+	void dockerWorks() {
+		var container = new MySQLContainer<>("mysql:8.4");
+		container.start();
+
+		System.out.println(container.getJdbcUrl());
+
+		container.stop();
 	}
 
 }
