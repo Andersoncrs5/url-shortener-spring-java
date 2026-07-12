941ca6a2d3b4e0aa24b6e11616e9662284ba55b7
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-07-12 13:17:59 -0300

    Set up UrlRedirectRuleCreatedEvent

---
+++ demo/src/main/java/com/write/api/application/dto/outbox/events/urlRedirectRule/UrlRedirectRuleCreatedEvent.java
@@ -0,0 +1,21 @@
+package com.write.api.application.dto.outbox.events.urlRedirectRule;
+
+import com.write.api.shared.validation.snowflake.IsId;
+
+import java.time.LocalDateTime;
+
+public record UrlRedirectRuleCreatedEvent(
+        Long id,
+        Long urlId,
+        LocalDateTime createdAt
+) {
+    public static UrlRedirectRuleCreatedEvent create(
+            @IsId Long id,
+            @IsId Long urlId,
+            LocalDateTime createdAt
+    ) {
+        return new UrlRedirectRuleCreatedEvent(
+                id, urlId, createdAt
+        );
+    }
+}
