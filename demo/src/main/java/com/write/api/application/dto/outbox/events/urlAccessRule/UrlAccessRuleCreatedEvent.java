44b311b7b63ec2eba15ed4678cdd2a6e004bd1c6
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-07-12 13:18:18 -0300

    .

--- demo/src/main/java/com/write/api/application/dto/outbox/events/urlAccessRule/UrlAccessRuleCreatedEvent.java
+++ demo/src/main/java/com/write/api/application/dto/outbox/events/urlAccessRule/UrlAccessRuleCreatedEvent.java
@@ -1,10 +1,9 @@
 package com.write.api.application.dto.outbox.events.urlAccessRule;
 
-import com.write.api.application.dto.outbox.events.url.UrlCreatedEvent;
 import com.write.api.core.domain.enums.UrlAccessRuleTypeEnum;
-import com.write.api.core.domain.enums.UrlStatusEnum;
 
 import java.time.LocalDateTime;
+
 public record UrlAccessRuleCreatedEvent(
         Long id,
         Long urlId,
