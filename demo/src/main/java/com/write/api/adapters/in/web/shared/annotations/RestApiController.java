cb0be693f8fadc61b3d7d8dc55fbe17b2bc00729
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-07-13 14:57:13 -0300

    Removed @Tag

--- demo/src/main/java/com/write/api/adapters/in/web/shared/annotations/RestApiController.java
+++ demo/src/main/java/com/write/api/adapters/in/web/shared/annotations/RestApiController.java
@@ -1,6 +1,5 @@
 package com.write.api.adapters.in.web.shared.annotations;
 
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.core.annotation.AliasFor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +13,6 @@ import java.lang.annotation.*;
 @Validated
 @RestController
 @RequestMapping
-@Tag(name = "API")
 public @interface RestApiController {
 
     @AliasFor(
