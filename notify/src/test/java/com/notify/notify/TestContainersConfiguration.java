0eb576b5f169e8e1ea95aa0a7f6d70d612dc8658
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-07-14 14:00:26 -0300

    Fix error port of kafka container

--- notify/src/test/java/com/notify/notify/TestContainersConfiguration.java
+++ notify/src/test/java/com/notify/notify/TestContainersConfiguration.java
@@ -4,24 +4,27 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.grafana.LgtmStackContainer;
 import org.testcontainers.kafka.KafkaContainer;
 import org.testcontainers.mysql.MySQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
+public class TestContainersConfiguration {
 
 	@Bean
 	@ServiceConnection
 	KafkaContainer kafkaContainer() {
-		return new KafkaContainer(DockerImageName.parse("apache/kafka-native:latest"));
+		KafkaContainer container = new KafkaContainer(DockerImageName.parse("apache/kafka-native:latest"));
+
+		container.setPortBindings(java.util.List.of("9092:9092"));
+
+		return container;
 	}
 
 	@Bean
 	@ServiceConnection
 	MySQLContainer mysqlContainer() {
-		return new MySQLContainer(DockerImageName.parse("mysql:latest"));
+		return new MySQLContainer(DockerImageName.parse("mysql:latest")).withDatabaseName("notify_db_test");
 	}
 
 	@Bean
